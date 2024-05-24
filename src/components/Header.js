335180df8cba237420fa61491644f9ef8af2e39a
Tony Xu <tonyxu74@hotmail.com>
2024-05-24 16:13:40 -0400

    small year and semantics fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,9 +9,9 @@ function Header(){
             </h1>
 
             <p className="Blurb">
-              Hello! I'm a first-year PhD candidate at the Department of Medical Biophysics at UofT supervised by Dr. Anne Martel and Dr. Maged Goubran. 
+              Hello! I'm a second-year PhD student at the Department of Medical Biophysics at UofT supervised by Dr. Anne Martel and Dr. Maged Goubran. 
               I recently graduated from UBC with a major in Electrical Engineering specializing in Biomedical Engineering and a minor in Physics. 
-              I'm interested in applying deep learning to a variety of areas including medical imaging and image reconstruction. 
+              I'm working on applying deep learning to a variety of areas including huge medical images and image reconstruction. 
               I enjoy learning new things, and am also an avid space enthusiast!
             </p>
             <p className="Blurb">
